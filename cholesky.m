18518e2b7dba6ab0a0c3a050096341f45c00da3d
LDC <ldc@taraza.ldc.usb.ve>
2018-10-24 11:02:07 -0400

    cholesky

---
+++ cholesky.m
@@ -0,0 +1,18 @@
+function L=cholesky(A)
+  n=length(A);
+  L=0*ones(n,n);
+  for k=1:1:n
+    suma=0;
+    for i=1:1:k-1
+      suma=suma+L(k,i)^2;
+    end
+    (k,k)=(A(k,k)-suma)^(1/2)
+    for i=k+1:1:n
+      suma2=0;
+      for m=1:1:k-1
+        suma2=suma2+L(i,m)*L(k,m);
+      end
+    end
+    L(i,k)=(A(i,k)-suma2)/L(k,k);
+  end
+endfunction
