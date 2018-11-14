6c6acb6d4af712ddd641d3835e9cd44e46760c35
invitado <jmoran071996@gmail.com>
2018-11-14 12:55:40 -0500

    potencia

---
+++ potencia.m
@@ -0,0 +1,13 @@
+function[x,k,r]=potencia(A,error,x,itmax)
+    for k=1:1:itmax
+        y=A*x;  % x^k+1 = Ax^k
+        r=y(1)/x(1);  % fi(x^(k+1))/fi(x^k)
+        if norm(x-(y/norm(y,inf)),inf)<error
+            return
+        end
+        x=y/norm(y,inf);
+    end
+end
+
+% Debo llamar la funcion asi: [x,k,r]=potencia(A,error,x,itmax)
+% para que me aparezcan los 3 resultados
