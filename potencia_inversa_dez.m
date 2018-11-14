3bd717a8bdfd2ede1e386416d1361e77fc7e8e2f
invitado <jmoran071996@gmail.com>
2018-11-14 13:33:00 -0500

    otra

---
+++ potencia_inversa_dez.m
@@ -0,0 +1,16 @@
+function [x,k,r]=potencia_inversa_dez(A,error,u,itmax)
+    n = length(A);
+    x = ones(n,1);
+    dezpla = u*eye(n,n);
+    A = A-dezpla
+    A = LU(A);
+    for k=1:1:itmax
+        y = sistemaLU(A,x);
+        r = y(1)/x(1);
+        if norm(x-(y/norm(y,inf)),inf)<error
+            return
+        end
+        x = y/norm(y,inf);
+    end
+    r = (1/r)+u;
+end
